daff2aafba40ac32cfd498fe7c13796b7576b861
indira2906 <indirairawatiputri6@gmail.com>
2023-09-05 08:13:38 +0700

    hello_world

--- lib/main.dart
+++ lib/main.dart
@@ -118,11 +118,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      //floatingActionButton: FloatingActionButton(
-      //onPressed: _incrementCounter,
-      //tooltip: 'Increment',
-      //child: const Icon(Icons.add),
-      //), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: FloatingActionButton(
+      onPressed: _incrementCounter,
+      tooltip: 'Increment',
+      : const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
